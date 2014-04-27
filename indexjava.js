8698d97ca219537aa2928ab9df6c99a13b4f9499
pommelpro <pommelpro@gmail.com>
2014-04-27 15:25:45 -0500

    index

---
+++ indexjava.js
@@ -0,0 +1,22 @@
+
+$(document).ready( function() {
+
+	$('body').hide().fadeIn(1000);
+	
+	$('#checkmark').click(function () {
+	    window.location.href = "check.html";
+	});
+	$('#heating').click(function () {
+	    window.location.href = "heat.html";
+	});
+	$('#flickr').click(function () {
+	    window.location.href = "main.html";
+	});
+	$('#swiper').click(function () {
+	    window.location.href = "swiper.html";
+	});
+	$('#tictactoe').click(function () {
+	    window.location.href = "tictactoe.html";
+	});
+
+});
