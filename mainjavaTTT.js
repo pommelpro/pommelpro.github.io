7d660b64823509d60eb163fac3036b661f46e4c0
pommelpro <pommelpro@gmail.com>
2014-05-04 23:17:18 -0500

    background of cells

--- mainjavaTTT.js
+++ mainjavaTTT.js
@@ -13,58 +13,13 @@ titles[7] = "Stuffed Animal";
 titles[8] = "Shorts";
 
 function fillgrid() {
-    for (var i = 0; i < buswait.length; i++) {
+    for (var i = 0; i < titles.length; i++) {
         $('#title' + i).html(titles[i]);
     }
 }
 
 
 
-var winner = 0;
-function WinGame() {
-    if (complete == 9) {
-        for (var j = 0; j < buswait.length; j++) {
-            $('#pic' + j).attr('src', endgame);
-            $('#title' + j).html("Cat's Game");
-        }
-        $('#switcher').html("Cat's Game");
-    }
-    else if (zeroA && oneA && twoA ||
-        threeA && fourA && fiveA ||
-        sixA && sevenA && eightA ||
-        zeroA && threeA && sixA ||
-        oneA && fourA && sevenA ||
-        twoA && fiveA && eightA ||
-        zeroA && fourA && eightA ||
-        twoA && fourA && sixA ||
-        zeroB && oneB && twoB ||
-        threeB && fourB && fiveB ||
-        sixB && sevenB && eightB ||
-        zeroB && threeB && sixB ||
-        oneB && fourB && sevenB ||
-        twoB && fiveB && eightB ||
-        zeroB && fourB && eightB ||
-        twoB && fourB && sixB) {
-        if (turns == 0) { winner = 10; }
-        else { winner = 9; }
-        for (var i = 0; i < buswait.length; i++) {
-            $('#pic' + i).attr('src', buswait[winner]);
-        }
-        if (winner == 10) { $('#switcher').html("Player 1 Wins!!!"); }
-        else if (winner == 9) { $('#switcher').html("Player 2 Wins!!!"); }
-        turns = 10;
-        count0 = 1;
-        count1 = 1;
-        count2 = 1;
-        count3 = 1;
-        count4 = 1;
-        count5 = 1;
-        count6 = 1;
-        count7 = 1;
-        count8 = 1;
-    }
-    turns = 10;
-}
 
 function getActiveTarget() {
     //find which cell has the class active target
@@ -78,7 +33,10 @@ $(document).ready(
 
 	function () {
 
-
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+////////////////Take picture and put it in cell////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 	    $(".spyTarget").click(function (event) { // triggers open file menu 
 	        $(this).addClass("activeTarget");
 	        $('#takePicture').trigger('click');
@@ -98,19 +56,16 @@ $(document).ready(
 	        activeTarget.removeClass('activeTarget');
 	        
 	    });
-
+///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+////////////////Take picture and put it in cell////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 
 	    $('body').hide().fadeIn(1000);
 	    
 	    $(function () {
 	        fillgrid();     //execute fill grid
-	        whosturn();
 	    });
-	    $('td').click(function () {
-	        $(this).toggleClass('on');
-	    });
-
-
 
 
 
