f24e99619419097c8c293cf354933be1e0a7d1ba
pommelpro <pommelpro@gmail.com>
2014-05-05 16:09:37 -0500

    image sizes

--- mainjavaTTT.js
+++ mainjavaTTT.js
@@ -1,18 +1,19 @@
-var titles = new Array();
+
 
 var endgame = "img/catgame.jpg";
-
-titles[0] = "Apple Computer";
-titles[1] = "iPhone";
-titles[2] = "Student";
-titles[3] = "Nicely Dressed Person";
-titles[4] = "Clock";
-titles[5] = "Jeans";
-titles[6] = "Android Phone";
-titles[7] = "Stuffed Animal";
-titles[8] = "Shorts";
+var switcher = 0;
 
 function fillgrid() {
+    var titles = new Array();
+    titles[0] = "Apple Computer";
+    titles[1] = "iPhone";
+    titles[2] = "Student";
+    titles[3] = "Nicely Dressed Person";
+    titles[4] = "Clock";
+    titles[5] = "Jeans";
+    titles[6] = "Android Phone";
+    titles[7] = "Stuffed Animal";
+    titles[8] = "Shorts";
     for (var i = 0; i < titles.length; i++) {
         $('#title' + i).html(titles[i]);
     }
@@ -39,7 +40,16 @@ $(document).ready(
 ///////////////////////////////////////////////////////////////////////////////////
 	    $(".spyTarget").click(function (event) { // triggers open file menu 
 	        $(this).addClass("activeTarget");
-	        $('#takePicture').trigger('click');
+	        var parid = $(this).parent().attr('id');
+	        if (switcher == 0) {
+	            $('#' + parid).css('background-color', 'red');
+	            switcher = 1;
+	        }
+	        else {
+	            $('#' + parid).css('background-color', 'green');
+	            switcher = 0;
+	        }
+	            $('#takePicture').trigger('click');
 	        return false;
 
 	    });
@@ -66,6 +76,9 @@ $(document).ready(
 	    $(function () {
 	        fillgrid();     //execute fill grid
 	    });
+
+
+
 
 
 
