346fb0b90e8e4a572e12a0c438f0779d36aa98e5
pommelpro <pommelpro@gmail.com>
2014-07-22 12:56:34 -0500

    take out abc

--- javaindex.js
+++ javaindex.js
@@ -3,13 +3,12 @@ $(document).ready( function() {
 
 	$('body').hide().fadeIn(1000);
 // generates a random number to see which game to play
-	var ranNum = Math.floor((Math.random() * 3) + 1);
+	var ranNum = Math.floor((Math.random() * 2) + 1);
 	console.log(ranNum);
 // if it's 1, 2, or 3 then go to corresponding file
 	$('#moveon').click(function () {
 	    if (ranNum == 1) { window.location.href = "htmlrandom.html" }
 	    else if (ranNum == 2) { window.location.href = "htmlspecific.html" }
-	    else if (ranNum == 3) { window.location.href = "htmlabc.html" }
     });
 
 });
