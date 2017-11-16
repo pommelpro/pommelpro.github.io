b8c275c95a3a8830c6b5cda2de2bb69bfb6ed711
pommelpro <pommelpro@gmail.com>
2017-11-15 21:54:52 -0600

    swipe carousel interaction

--- main.js
+++ main.js
@@ -15,55 +15,83 @@ window.onload = function() {
   document.getElementById("navRight").addEventListener("click", function() {
     NavigateCarousel(true);
   });
+
+  let fingerStartPosition = 0;
+  document.getElementById("innerCarousel").addEventListener("touchstart", function(e) {
+    fingerStartPosition = onTouchStart(e);
+  })
+  document.getElementById("innerCarousel").addEventListener("touchend", function(e) {
+    onTouchEnd(e, fingerStartPosition);
+  })
+}
+
+function onTouchStart(e) {
+  return e.touches[0].clientX;
+}
+
+function onTouchEnd(e, fingerStartPosition) {
+  const fingerEndPosition = e.changedTouches[0].clientX;
+  if (fingerEndPosition - fingerStartPosition < -50) {
+    NavigateCarousel(true);
+  } else if (fingerEndPosition - fingerStartPosition > 50) {
+    NavigateCarousel(false);
+  }
 }
 
 function NavigateCarousel(isNavRight) {
   const carousel = document.getElementById("innerCarousel");
-  const carouselWidth = carousel.clientWidth;
   const isMobile = window.innerWidth <= 900;
   const maxScrollLeft = carousel.scrollWidth - carousel.clientWidth;
   const scrollLeft = carousel.scrollLeft;
   if (isNavRight) {
-    document.getElementById("navLeft").classList.remove("hiddenToggle");
-    var scrolling = setInterval(function(){
-      if (isMobile) {
-        carousel.scrollLeft += 10;
-        if (carousel.scrollLeft >= scrollLeft + 290  || carousel.scrollLeft === maxScrollLeft) {
-          clearInterval(scrolling);
-          if (carousel.scrollLeft === maxScrollLeft) {
-            document.getElementById("navRight").classList.add("hiddenToggle");
-          }
-        }
-      } else {
-        carousel.scrollLeft += 10;
-        if (carousel.scrollLeft >= scrollLeft + 190 || carousel.scrollLeft === maxScrollLeft) {
-          clearInterval(scrolling);
-          if (carousel.scrollLeft === maxScrollLeft) {
-            document.getElementById("navRight").classList.add("hiddenToggle");
-          }
-        }
-      }
-    }, 15);
+    onNavRight(carousel, isMobile, maxScrollLeft, scrollLeft);
   } else {
-    document.getElementById("navRight").classList.remove("hiddenToggle");
-    var scrolling = setInterval(function(){
-      if (isMobile) {
-        carousel.scrollLeft -= 10;
-        if (carousel.scrollLeft <= scrollLeft - 290  || carousel.scrollLeft === 0) {
-          clearInterval(scrolling);
-          if (carousel.scrollLeft === 0) {
-            document.getElementById("navLeft").classList.add("hiddenToggle");
-          }
-        }
-      } else {
-        carousel.scrollLeft -= 10;
-        if (carousel.scrollLeft <= scrollLeft - 190 || carousel.scrollLeft === 0) {
-          clearInterval(scrolling);
-          if (carousel.scrollLeft === 0) {
-            document.getElementById("navLeft").classList.add("hiddenToggle");
-          }
+    onNavLeft(carousel, isMobile, maxScrollLeft, scrollLeft);
+  }
+}
+
+function onNavRight(carousel, isMobile, maxScrollLeft, scrollLeft) {
+  document.getElementById("navLeft").classList.remove("hiddenToggle");
+  var scrolling = setInterval(function(){
+    if (isMobile) {
+      carousel.scrollLeft += 10;
+      if (carousel.scrollLeft >= scrollLeft + 290  || carousel.scrollLeft === maxScrollLeft) {
+        clearInterval(scrolling);
+        if (carousel.scrollLeft === maxScrollLeft) {
+          document.getElementById("navRight").classList.add("hiddenToggle");
         }
       }
-    }, 15);
-  }
+    } else {
+      carousel.scrollLeft += 10;
+      if (carousel.scrollLeft >= scrollLeft + 190 || carousel.scrollLeft === maxScrollLeft) {
+        clearInterval(scrolling);
+        if (carousel.scrollLeft === maxScrollLeft) {
+          document.getElementById("navRight").classList.add("hiddenToggle");
+        }
+      }
+    }
+  }, 15);
+}
+
+function onNavLeft(carousel, isMobile, maxScrollLeft, scrollLeft) {
+  document.getElementById("navRight").classList.remove("hiddenToggle");
+  var scrolling = setInterval(function(){
+    if (isMobile) {
+      carousel.scrollLeft -= 10;
+      if (carousel.scrollLeft <= scrollLeft - 290  || carousel.scrollLeft === 0) {
+        clearInterval(scrolling);
+        if (carousel.scrollLeft === 0) {
+          document.getElementById("navLeft").classList.add("hiddenToggle");
+        }
+      }
+    } else {
+      carousel.scrollLeft -= 10;
+      if (carousel.scrollLeft <= scrollLeft - 190 || carousel.scrollLeft === 0) {
+        clearInterval(scrolling);
+        if (carousel.scrollLeft === 0) {
+          document.getElementById("navLeft").classList.add("hiddenToggle");
+        }
+      }
+    }
+  }, 15);
 }
